dc6b997d5146befec92d18fe0c9c8b7f8c5f5136
hiddeneyes02 <9453054+hiddeneyes02@users.noreply.github.com>
2023-06-05 15:54:50 +0100

    test

--- src/main/kotlin/com/dzboot/android_translator/MainApp.kt
+++ src/main/kotlin/com/dzboot/android_translator/MainApp.kt
@@ -6,7 +6,7 @@ import javafx.scene.Parent
 import javafx.scene.Scene
 import javafx.stage.Stage
 
-
+//test
 class MainApp : Application() {
     private var mainController: MainController? = null
 
